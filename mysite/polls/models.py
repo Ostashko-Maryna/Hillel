1816eb71507f1fee70deb715d21484f37c208f5f
Ostashko-Maryna <m_ostashko@ukr.net>
2020-04-10 09:00:27 +0300

    some changes in models.py and views.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -11,17 +11,17 @@ class Question(models.Model):
     views = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.question_text
+        return "{} publicated at {}".format(self.question_text, self.pub_date)
 
     @property
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text
+        return "{} voted {} times".format(self.choice_text, self.votes)
 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,28 @@
 # from django.shortcuts import render
-
+from polls.models import Question#, Choice
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return HttpResponse("You're looking at question: {}.".format(question))
+
+def results(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	# print(dir(question))
+	# choices = Choice.objects.filter(question=question)
+	choises = question.choices.all()
+	output = ', '.join([str(c) for c in choises])
+	return HttpResponse(output)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def vote_user(request, question_id, user_id):
+    return HttpResponse("You're voting on question {} for user {}.".format(question_id, user_id) )
+
